643b4cf95c74d84f0f9aae91beb48841abafd3e5
zhangrp <zhangrp@bairuitech.com>
2021-05-12 16:44:02 +0800

    增加网格布局
    Change-Id: If1927af7809ffa6e212fb6c87c0f59043cd4d366

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,13 @@ export const Home = () => {
   return (
     <div className='wrapper'>
       <header />
+      <main>
+          <section className="section1" />
+          <section className="section2" />
+          <section className="section3" />
+          <section className="section4" />
+          <section className="section5" />
+      </main>
     </div>
   );
 };
