f82ff2c9381f3045978d8f00336a9458370e81e2
zhangrp <zhangrp@bairuitech.com>
2021-05-31 16:24:33 +0800

    案件破获排名柱形图增加颜色渐变
    Change-Id: I75702fdf50e73ffc3a0f58411d9d489b0250192b

--- src/components/chart-1.tsx
+++ src/components/chart-1.tsx
@@ -54,9 +54,7 @@ export const Chart1 = () => {
     return (
         <div className='管辖统计 bordered'>
             <h2>案发派出所管辖统计</h2>
-            <div ref={divRef} className="chart">
-
-            </div>
+            <div ref={divRef} className="chart" />
         </div>
     )
 }
