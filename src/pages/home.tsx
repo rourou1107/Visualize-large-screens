2a8c77b2c6d1968a0b170eed14dd2ea45c6e24e4
zhangrp <zhangrp@bairuitech.com>
2021-05-31 11:28:02 +0800

    案发派出所管辖统计柱状图完成
    Change-Id: I38b10b983f4a07810a769d434a7d0f53af53a2fc

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,81 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './home.scss';
+import * as echarts from 'echarts';
 
+const px = n => n / 2420 * (window as any).pageWidth // 设计稿中元素的大小 / 设计稿的宽度 * 页面的宽度
 export const Home = () => {
-  return (
-    <div className='home'>
-      <header />
-      <main>
-          <section className="section1" />
-          <section className="section2" />
-          <section className="section3" />
-          <section className="section4" />
-          <section className="section5" />
-      </main>
-    </div>
-  );
+    const divRef = useRef(null)
+    useEffect(() => {
+        const myChart = echarts.init(divRef.current);
+        const option = {
+            textStyle: {
+                fontSize: px(12),
+                color: '#79839E'
+            },
+            grid: {
+                left: px(40),
+                top: px(40),
+                right: px(40),
+                bottom: px(40)
+            },
+            xAxis: {
+                type: 'category',
+                data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
+                axisLabel: {
+                    fontSize: px(12),
+                    formatter(val) {
+                        if (val.length > 2) {
+                            const array = val.split('')
+                            array.splice(2, 0 , '\n')
+                            return array.join('')
+                        } else {
+                            return val
+                        }
+                    }
+                },
+                axisLine: {
+                    // 显示 x 轴的轴线
+                    show: true,
+                    lineStyle: {color: '#083B70'},
+                },
+                axisTick: {show: false}
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    fontSize: px(12)
+                },
+                axisLine: {
+                    // 显示 y 轴的轴线
+                    show: true,
+                    lineStyle: {color: '#083B70'}
+                },
+                splitLine: {show: false} // 不显示坐标轴分割线
+            },
+            series: [{
+                data: [20, 79, 39, 48, 21, 10, 58],
+                type: 'bar'
+            }]
+        };
+        myChart.setOption(option);
+    }, [])
+    return (
+        <div className='home'>
+          <header />
+          <main>
+              <section className="section1">
+                  <div className='管辖统计 bordered'>
+                      <h2>案发派出所管辖统计</h2>
+                      <div ref={divRef} className="chart">
+
+                      </div>
+                  </div>
+              </section>
+              <section className="section2 bordered" />
+              <section className="section3 bordered" />
+              <section className="section4 bordered" />
+              <section className="section5 bordered" />
+          </main>
+        </div>
+    );
 };
